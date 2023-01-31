30a6319ea09111783267a67cf9f177fd41fc2604
YuliaLoseva <losevaulia59@gmail.com>
2023-01-31 12:47:26 +0300

    update: added new function

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world! whats up?");
+        System.out.println("Norm");
+        System.out.println("chem zanimaeshs9?");
+        System.out.println("ni4em");
+    }
+}
